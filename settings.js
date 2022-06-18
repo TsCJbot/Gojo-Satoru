c41b70d63a9ed7f2dd477b7fffdd19e057e74ada
TsCJbot <107740890+TsCJbot@users.noreply.github.com>
2022-06-18 18:26:14 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -19,17 +19,17 @@ global.APIKeys = {
 }
 
 //customize settings\\
-global.owner = ['Your Number']
-global.premium = ['Your Number']
-global.ownernomer = 'Your Number'
-global.ownername = '🐦Star Boy Nexus'
-global.botname = 'Gojo-Satoru'
+global.owner = ['71108806']
+global.premium = ['711088065']
+global.ownernomer = '711088065'
+global.ownername = '🐦CJ Boy Nexus'
+global.botname = 'CJ-Bot'
 global.footer = '©NexusNwInc.'
 global.ig = 'https://github.com/nexusNw'
 global.region = 'India, South India, Kerala'
 global.sc = 'https://github.com/nexusNw/Gojo-Satoru'
-global.myweb = 'https://youtube.com/channel/UCqoUjPvDdb0kjXNYdvPPpHQ'
-global.packname = 'Gojo-Satoru'
+global.myweb = 'https://youtube.com/channel/UC_LXIL1NxX8EBs10v_Zu4lw'
+global.packname = 'MADE by -Cj Bot'
 global.author = '🐦Star Boy Nexus'
 global.sessionName = 'session'
 global.prefa = ['','!','.','🐦','🐤','🗿']
